509f580512998097351dacf3325002fb822e2390
iamfarax <98969818+iamfarax@users.noreply.github.com>
2023-06-20 22:57:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let navbar = document.getElementsByClassName(".navbar");
+
+let burger = document.getElementsByClassName(".burger").addEventListener('click', () => {
+    navbar.style.display = "block"
+})
